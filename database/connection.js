b26e1936b13f436c5e8104b8c42e37437ea52f70
Marcelo Rendón <81325245+MarceloRendon@users.noreply.github.com>
2024-01-22 18:30:56 -0300

    Update connection from local to atlas

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
+//Conexión local
+//mongodb://127.0.0.1:27017/mi_blog
+
+//Conexión atlas
+//mongodb+srv://mrendondemelo:<password>@cluster0.fdndmue.mongodb.net/mi_blog
+//Username: mrendondemelo
+//Password: 1Q4Wt60bwdetmDHr
+
 
 const connection = async () => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/mi_blog")
+        await mongoose.connect("mongodb+srv://mrendondemelo:1Q4Wt60bwdetmDHr@cluster0.fdndmue.mongodb.net/mi_blog")
 
         console.log("Conectado correctamente a la base de datos")
 
